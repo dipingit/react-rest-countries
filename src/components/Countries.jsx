75ada7b8bc3437332eff80c5887e1f09fc9c0a75
Dip Roy <diproyf87@gmail.com>
2025-01-07 00:27:55 -0400

    added visited countries feature

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -4,18 +4,29 @@ import './Countries.css';
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
         .then(res =>  res.json())
         .then(data => setCountries(data))
         
     }, []);
+
+    const handleVisitedCountries = (country) => {
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+    }
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <p>Visited Countries: {
+                visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                
+                } </p>
+                
             <div className='country-container'>
                 {
-                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
+                    countries.map(country => <Country key={country.cca3} handleVisitedCountries={handleVisitedCountries} country={country}></Country>)
                 }
             </div>
             
